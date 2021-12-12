85d665ea8813b5989e2d548046094a3bc1cf16d9
luisaW-stack <95079218+luisaW-stack@users.noreply.github.com>
2021-12-12 21:27:34 +0100

    Add files via upload

---
+++ src/components/NodeCard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="card-group">
+    <b-card-group deck class="card-group">
+      <b-card  border-variant="light" :header="getName(node.name)" class="text-center" :footer="getFooter(node.text)" bg-variant="info">
+      </b-card>
+    </b-card-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NodeCard",
+  methods: {
+    getImgUrl(imagePath) {
+      return ("../assets/" + imagePath);
+    },
+    getName(name) {
+      return name;
+    },
+    getFooter(footer) {
+      return footer;
+    }
+  },
+  props: { 
+    node: {
+      type: Object,
+      required: true,
+    },
+    mainProps: {
+
+    }
+  }
+};
+</script>
+
+<style scoped>
+  .card-group {
+    display: inline-block;
+    margin: 10px;
+  }
+</style>
---
+++ src/components/NodeContainer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <node-card
+      v-for="node in nodes"
+      v-bind:key="node.id"
+      v-bind:node="node"> 
+      </node-card>
+    </div>
+
+</template>
+
+<script>
+import NodeCard from './NodeCard.vue';
+
+export default {
+  name: "NodeContainer",
+  components: {
+    NodeCard, 
+  },
+  props: { // to get the nodes array from 'App'
+    nodes: {
+      type: Array,
+      required: true, 
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
