72fac4cb123153b99d4fb3bf2eadee1ab875ddc1
luisaW-stack <95079218+luisaW-stack@users.noreply.github.com>
2021-12-13 16:24:53 +0100

    src folder

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <input type="search" v-model="node" list="nodes" placeholder="Add new node..." @keyup.enter="addNode">
+    <datalist id="nodes">
+      <!-- <option :v-for="node in nodes">{{node.name}}</option> -->
+      <option value="Data Generator"></option>
+      <option value="CASE Switch Data (Start)"></option>
+      <option value="Cluster Assigner"></option>
+    </datalist>
+    <node-container :nodes="nodes"/>
+  </div>
+</template>
+
+<script>
+import NodeContainer from "./components/NodeContainer.vue";
+
+export default {
+  name: "App",
+  components: {
+    NodeContainer,
+  },
+  methods: {
+    addNode: function() {
+      this.nodes.push({
+        id: this.idCounter++,
+        name: this.name,
+        text: this.text,
+      });
+      console.log(this.nodes);
+    },
+  },
+
+  
+  data() {
+    return {
+      idCounter: 2,
+      mainProps: { width: 100, height: 100},
+
+      nodes: [
+        {
+          id: 0,
+          name: "Data Generator",
+          text: "Source",
+        },
+        {
+          id: 1,
+          name: "CASE Switch Data (Start)",
+          text: "Manipulator",
+        },
+        {
+          id: 2,
+          name: "Cluster Assigner",
+          text: "Predictor",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-top: 20px;
+  margin-left: 20px;
+}
+</style>
